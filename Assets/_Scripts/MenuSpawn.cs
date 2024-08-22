57c2620eea667878faf457aab479d986ae69f211
Davwolf <davidosorio.mct@gmail.com>
2024-08-22 00:06:21 -0600

    [ADD] MapWithPins
    -Added a Map for the Location principal Button
    -Fixed Object Spawn Orientation

--- Assets/_Scripts/MenuSpawn.cs
+++ Assets/_Scripts/MenuSpawn.cs
@@ -9,6 +9,9 @@ public class MenuSpawn : MonoBehaviour
     public float offsetX=0;
     public float offsetY=0;
     public float offsetZ = 0.5f;
+    public int rotOffsetX = 0;
+    public int rotOffsetY = 0;
+    public int rotOffsetZ = 0;
 
     private Vector3 headPosition;
 
@@ -23,19 +26,36 @@ public class MenuSpawn : MonoBehaviour
     {
         Vector3 referencePosition = OvrHMD.transform.position;
 
-        // Calcular la nueva posición usando la dirección hacia adelante del objeto de referencia
+        //POSITION
         Vector3 forward = OvrHMD.transform.forward * offsetZ;
         Vector3 right = OvrHMD.transform.right * offsetX;
         Vector3 up = OvrHMD.transform.up * offsetY;
 
-        // Sumar los offsets a la posición de referencia
         Vector3 newPosition = referencePosition + forward + right + up;
 
+        //ROTATION
+        Quaternion referenceRotation = Quaternion.Euler(rotOffsetX,OvrHMD.transform.eulerAngles.y+rotOffsetY,rotOffsetZ);
+
         // Instanciar el objeto en la posición calculada
-        GameObject newObject = Instantiate(SpawnObject, newPosition, Quaternion.identity);
+        GameObject newObject = Instantiate(SpawnObject, newPosition, referenceRotation);
 
         // El objeto no será hijo del objeto de referencia, sino que estará en la raíz de la jerarquía
-        newObject.transform.SetParent(null);
+        //newObject.transform.SetParent(null);
+    }
+
+    public void SpawnMap(bool activate)
+    {
+        bool isMap;
+        if(GameObject.FindWithTag("Map"))
+            isMap = true;
+        else
+            isMap = false;
+
+        if(activate && !isMap)
+            SpawnInFront();
+        else if(!activate && isMap)
+            Destroy(GameObject.FindWithTag("Map"));
+
     }
 
 }
